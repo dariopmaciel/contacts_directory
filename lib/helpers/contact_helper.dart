5102ec1fa28493d5b97823c20889d3c00683b1c8
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-01-05 13:40:17 -0300

    tabela

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -6,12 +6,12 @@ import 'package:path/path.dart';
 import 'dart:async';
 
 //nome das colunas
-final String contactTable = "contactTable";
-final String idColumns = "idColumn";
-final String nameColumn = "nameColumn";
-final String emailColumn = "emailColumn";
-final String phoneColumn = "phoneColumn";
-final String imgColumn = "imgColumn";
+String contactTable = "contactTable";
+String idColumns = "idColumn";
+String nameColumn = "nameColumn";
+String emailColumn = "emailColumn";
+String phoneColumn = "phoneColumn";
+String imgColumn = "imgColumn";
 
 //criação de BD
 class ContactHelper {
@@ -46,6 +46,61 @@ class ContactHelper {
           'CREATE TABLE $contactTable($idColumns INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)');
     });
   }
+
+//C
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+//R
+  Future<Contact?> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColumns, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumns=?",
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+//U
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(),
+        where: "$idColumns = ?", whereArgs: [contact.id]);
+  }
+
+//D
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+    return await dbContact
+        .delete(contactTable, where: "$idColumns = ?", whereArgs: [id]);
+  }
+
+//Carregar todos os contatos
+  Future<List> getAllContacts() async {
+    Database dbContact = await db;
+    //lista de mapas
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    //declaração de lista de contatos
+    List<Contact> listContact = [];
+    //para cada mapa da minha lista de mapa eu transformo ele em um contato e adiciono em uma lista de contatos
+    for (Map m in listMap) {
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+
+  Future<int?> getNumber() async {
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(
+        await dbContact.rawQuery("SELECT COUNT(*)FROM $contactTable"));
+  }
 }
 
 // id, name, email, phone, img
@@ -59,7 +114,7 @@ class Contact {
 
   //Transforma os dados declarados em um Mapa
   Map toMap() {
-    Map<String, dynamic> map = {
+    Map<String?, dynamic> map = {
       nameColumn: name,
       emailColumn: email,
       phoneColumn: phone,
