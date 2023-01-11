a4d337e103b548ae70dbf04d23102af4c337960d
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-01-11 08:51:45 -0300

    11-01-23

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:contacts_directory/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
   ));
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contacts_directory/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -11,25 +13,128 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
 
+  List<dynamic> contacts = []; //List();
+
   @override
   void initState() {
     super.initState();
 
-    // Contact c = Contact();
-    // c.name = "Dario";
-    // c.email = "dario@dario";
-    // c.phone = "18981996400";
-    // c.img = "imgTest";
+    Contact c = Contact();
+    c.name = "Marcos";
+    c.email = "marcos@marcos.com";
+    c.phone = "6516546464";
+    c.img = "imgTest";
 
-    //helper.saveContact(c);
+    helper.saveContact(c);
+
+    helper.getAllContacts().then((list) {
+      print(list);
+      // setState(() {
+      //   contacts = list;
+      // });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: const Drawer(),
-      body: Container(color: Colors.pink),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text(
+                "Dario",
+              ),
+              accountEmail: Text(
+                "dariodepaulamaciel@hotmail.com",
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.greenAccent,
+                child: Text(
+                  "D",
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                "Este projeto foi criado mais um experimento de aprendizagem. \n\nEntão----. \n\nNovas funcionalidades, estarão disponiveis em breve.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                "\nObrigado.",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text("Contatos"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: contacts.length,
+        itemBuilder: ((context, index) {
+          return _contactCart(context, index);
+        }),
+      ),
+    );
+  }
+
+//CARTÃO DE CONTATO
+  Widget _contactCart(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80,
+                height: 80,
+                decoration: const BoxDecoration(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      contacts[index].name ?? "",
+                      style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
