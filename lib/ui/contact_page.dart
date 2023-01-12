414e54fe855dd1849fb468a241ac03ee663c964d
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-01-12 15:03:40 -0300

    .

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contacts_directory/helpers/contact_helper.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,8 +15,55 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  late Contact _editedContact;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  // image: DecorationImage(
+                  //   image: FileImage(File("images/person.png")),
+                  // ),
+                  image: DecorationImage(
+                    image: contacts[index].img != null
+                        ? FileImage(File(contacts[index].img))
+                        : FileImage(File("images/person.png")),
+                  ),
+                ),
+              ),,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
